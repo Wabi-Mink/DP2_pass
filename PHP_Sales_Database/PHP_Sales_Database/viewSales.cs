630d03b7385bd0676955583ec4412c84771ddd8e
Tienesphus <tienesphus@gmail.com>
2020-09-26 22:27:31 +1000

    Updated viewSales.cs

---
+++ PHP_Sales_Database/PHP_Sales_Database/viewSales.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+
+namespace PHP_Sales_Database
+{
+    public partial class viewSales : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public viewSales()
+        {
+            InitializeComponent();
+        }
+
+        private void mainTitleBar_panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void mainClose_panel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mainMinimise_panel_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+        private void viewSalesMinimise_panel_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void mainBack_label_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
