9a83aa9326bbb936a230fcf094d381524ee9ca02
Tienesphus <tienesphus@gmail.com>
2020-09-26 22:15:47 +1000

    Updated viewSales.cs

---
+++ PHP_Sales_Database/PHP_Sales_Database/viewSales.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace PHP_Sales_Database
+{
+    partial class viewSales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.viewSalesExport_button = new System.Windows.Forms.Button();
+            this.viewSalesSettings_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.viewSalesReports_label = new System.Windows.Forms.Label();
+            this.viewSalesInventory_label = new System.Windows.Forms.Label();
+            this.viewSalesSales_label = new System.Windows.Forms.Label();
+            this.viewSalesInbox_label = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.viewSalesTitleBar_panel = new System.Windows.Forms.Panel();
+            this.mainBack_label = new System.Windows.Forms.Label();
+            this.viewSalesMinimise_panel = new System.Windows.Forms.Label();
+            this.viewSalesClose_panel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.viewSalesAdd_button = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.viewSalesTitleBar_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.viewSalesExport_button);
+            this.panel1.Controls.Add(this.viewSalesSettings_button);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-1, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(176, 543);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox3.Location = new System.Drawing.Point(30, 487);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(34, 36);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox2.Location = new System.Drawing.Point(30, 447);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 36);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // viewSalesExport_button
+            // 
+            this.viewSalesExport_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.viewSalesExport_button.Location = new System.Drawing.Point(70, 493);
+            this.viewSalesExport_button.Name = "viewSalesExport_button";
+            this.viewSalesExport_button.Size = new System.Drawing.Size(89, 30);
+            this.viewSalesExport_button.TabIndex = 3;
+            this.viewSalesExport_button.Text = "Export";
+            this.viewSalesExport_button.UseVisualStyleBackColor = true;
+            // 
+            // viewSalesSettings_button
+            // 
+            this.viewSalesSettings_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.viewSalesSettings_button.Location = new System.Drawing.Point(70, 454);
+            this.viewSalesSettings_button.Name = "viewSalesSettings_button";
+            this.viewSalesSettings_button.Size = new System.Drawing.Size(89, 29);
+            this.viewSalesSettings_button.TabIndex = 2;
+            this.viewSalesSettings_button.Text = "Settings";
+            this.viewSalesSettings_button.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(15, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 143);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.viewSalesReports_label);
+            this.panel2.Controls.Add(this.viewSalesInventory_label);
+            this.panel2.Controls.Add(this.viewSalesSales_label);
+            this.panel2.Controls.Add(this.viewSalesInbox_label);
+            this.panel2.Location = new System.Drawing.Point(175, 39);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(166, 543);
+            this.panel2.TabIndex = 3;
+            // 
+            // viewSalesReports_label
+            // 
+            this.viewSalesReports_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.viewSalesReports_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.viewSalesReports_label.Location = new System.Drawing.Point(6, 164);
+            this.viewSalesReports_label.Name = "viewSalesReports_label";
+            this.viewSalesReports_label.Size = new System.Drawing.Size(152, 29);
+            this.viewSalesReports_label.TabIndex = 5;
+            this.viewSalesReports_label.Text = "Reports";
+            this.viewSalesReports_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // viewSalesInventory_label
+            // 
+            this.viewSalesInventory_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.viewSalesInventory_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.viewSalesInventory_label.Location = new System.Drawing.Point(6, 115);
+            this.viewSalesInventory_label.Name = "viewSalesInventory_label";
+            this.viewSalesInventory_label.Size = new System.Drawing.Size(152, 29);
+            this.viewSalesInventory_label.TabIndex = 4;
+            this.viewSalesInventory_label.Text = "Inventory";
+            this.viewSalesInventory_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // viewSalesSales_label
+            // 
+            this.viewSalesSales_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.viewSalesSales_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.viewSalesSales_label.Location = new System.Drawing.Point(6, 67);
+            this.viewSalesSales_label.Name = "viewSalesSales_label";
+            this.viewSalesSales_label.Size = new System.Drawing.Size(152, 29);
+            this.viewSalesSales_label.TabIndex = 3;
+            this.viewSalesSales_label.Text = "Sales";
+            this.viewSalesSales_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // viewSalesInbox_label
+            // 
+            this.viewSalesInbox_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.viewSalesInbox_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.viewSalesInbox_label.Location = new System.Drawing.Point(6, 18);
+            this.viewSalesInbox_label.Name = "viewSalesInbox_label";
+            this.viewSalesInbox_label.Size = new System.Drawing.Size(152, 29);
+            this.viewSalesInbox_label.TabIndex = 2;
+            this.viewSalesInbox_label.Text = "Inbox";
+            this.viewSalesInbox_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.viewSalesAdd_button);
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Location = new System.Drawing.Point(339, 39);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(577, 543);
+            this.panel3.TabIndex = 4;
+            // 
+            // viewSalesTitleBar_panel
+            // 
+            this.viewSalesTitleBar_panel.BackColor = System.Drawing.SystemColors.Highlight;
+            this.viewSalesTitleBar_panel.Controls.Add(this.mainBack_label);
+            this.viewSalesTitleBar_panel.Controls.Add(this.viewSalesMinimise_panel);
+            this.viewSalesTitleBar_panel.Controls.Add(this.viewSalesClose_panel);
+            this.viewSalesTitleBar_panel.Location = new System.Drawing.Point(-1, -1);
+            this.viewSalesTitleBar_panel.Name = "viewSalesTitleBar_panel";
+            this.viewSalesTitleBar_panel.Size = new System.Drawing.Size(918, 43);
+            this.viewSalesTitleBar_panel.TabIndex = 5;
+            // 
+            // mainBack_label
+            // 
+            this.mainBack_label.AutoSize = true;
+            this.mainBack_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainBack_label.Location = new System.Drawing.Point(12, 12);
+            this.mainBack_label.Name = "mainBack_label";
+            this.mainBack_label.Size = new System.Drawing.Size(16, 17);
+            this.mainBack_label.TabIndex = 4;
+            this.mainBack_label.Text = "<";
+            this.mainBack_label.Click += new System.EventHandler(this.mainBack_label_Click);
+            // 
+            // viewSalesMinimise_panel
+            // 
+            this.viewSalesMinimise_panel.AutoSize = true;
+            this.viewSalesMinimise_panel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.viewSalesMinimise_panel.Location = new System.Drawing.Point(865, 12);
+            this.viewSalesMinimise_panel.Name = "viewSalesMinimise_panel";
+            this.viewSalesMinimise_panel.Size = new System.Drawing.Size(13, 17);
+            this.viewSalesMinimise_panel.TabIndex = 2;
+            this.viewSalesMinimise_panel.Text = "-";
+            this.viewSalesMinimise_panel.Click += new System.EventHandler(this.viewSalesMinimise_panel_Click);
+            // 
+            // viewSalesClose_panel
+            // 
+            this.viewSalesClose_panel.AutoSize = true;
+            this.viewSalesClose_panel.Cursor = System.Windows.Forms.Cursors.No;
+            this.viewSalesClose_panel.Location = new System.Drawing.Point(888, 12);
+            this.viewSalesClose_panel.Name = "viewSalesClose_panel";
+            this.viewSalesClose_panel.Size = new System.Drawing.Size(17, 17);
+            this.viewSalesClose_panel.TabIndex = 0;
+            this.viewSalesClose_panel.Text = "X";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 49;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(537, 349);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // viewSalesAdd_button
+            // 
+            this.viewSalesAdd_button.Location = new System.Drawing.Point(19, 383);
+            this.viewSalesAdd_button.Name = "viewSalesAdd_button";
+            this.viewSalesAdd_button.Size = new System.Drawing.Size(537, 54);
+            this.viewSalesAdd_button.TabIndex = 1;
+            this.viewSalesAdd_button.Text = "ADD";
+            this.viewSalesAdd_button.UseVisualStyleBackColor = true;
+            // 
+            // viewSales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 582);
+            this.Controls.Add(this.viewSalesTitleBar_panel);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "viewSales";
+            this.Text = "viewSales";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.viewSalesTitleBar_panel.ResumeLayout(false);
+            this.viewSalesTitleBar_panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel viewSalesTitleBar_panel;
+        private System.Windows.Forms.Label viewSalesInbox_label;
+        private System.Windows.Forms.Label viewSalesReports_label;
+        private System.Windows.Forms.Label viewSalesInventory_label;
+        private System.Windows.Forms.Label viewSalesSales_label;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button viewSalesExport_button;
+        private System.Windows.Forms.Button viewSalesSettings_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label viewSalesMinimise_panel;
+        private System.Windows.Forms.Label viewSalesClose_panel;
+        private System.Windows.Forms.Label mainBack_label;
+        private System.Windows.Forms.Button viewSalesAdd_button;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
