1f7e16960acc0b817fdd39d7086ea507e20aef39
Tienesphus <tienesphus@gmail.com>
2020-09-26 21:45:39 +1000

    Updated mainForm's UI.

--- PHP_Sales_Database/PHP_Sales_Database/main.Designer.cs
+++ PHP_Sales_Database/PHP_Sales_Database/main.Designer.cs
@@ -28,12 +28,263 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mainWelcome_label = new System.Windows.Forms.Label();
+            this.mainContent_label = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.mainExport_button = new System.Windows.Forms.Button();
+            this.mainSettings_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.mainReports_label = new System.Windows.Forms.Label();
+            this.mainInventory_label = new System.Windows.Forms.Label();
+            this.mainSales_label = new System.Windows.Forms.Label();
+            this.mainInbox_label = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.mainTitleBar_panel = new System.Windows.Forms.Panel();
+            this.mainClose_panel = new System.Windows.Forms.Label();
+            this.mainMinimise_panel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.mainTitleBar_panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainWelcome_label
+            // 
+            this.mainWelcome_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainWelcome_label.Location = new System.Drawing.Point(17, 20);
+            this.mainWelcome_label.Name = "mainWelcome_label";
+            this.mainWelcome_label.Size = new System.Drawing.Size(542, 39);
+            this.mainWelcome_label.TabIndex = 0;
+            this.mainWelcome_label.Text = "mainWelcome_label";
+            this.mainWelcome_label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // mainContent_label
+            // 
+            this.mainContent_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainContent_label.Location = new System.Drawing.Point(18, 76);
+            this.mainContent_label.Name = "mainContent_label";
+            this.mainContent_label.Size = new System.Drawing.Size(541, 447);
+            this.mainContent_label.TabIndex = 1;
+            this.mainContent_label.Text = "mainWelcome_label";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.mainExport_button);
+            this.panel1.Controls.Add(this.mainSettings_button);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-1, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(176, 543);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox3.Location = new System.Drawing.Point(30, 487);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(34, 36);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox2.Location = new System.Drawing.Point(30, 447);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 36);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // mainExport_button
+            // 
+            this.mainExport_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainExport_button.Location = new System.Drawing.Point(70, 493);
+            this.mainExport_button.Name = "mainExport_button";
+            this.mainExport_button.Size = new System.Drawing.Size(89, 30);
+            this.mainExport_button.TabIndex = 3;
+            this.mainExport_button.Text = "Export";
+            this.mainExport_button.UseVisualStyleBackColor = true;
+            // 
+            // mainSettings_button
+            // 
+            this.mainSettings_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainSettings_button.Location = new System.Drawing.Point(70, 454);
+            this.mainSettings_button.Name = "mainSettings_button";
+            this.mainSettings_button.Size = new System.Drawing.Size(89, 29);
+            this.mainSettings_button.TabIndex = 2;
+            this.mainSettings_button.Text = "Settings";
+            this.mainSettings_button.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(15, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 143);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.mainReports_label);
+            this.panel2.Controls.Add(this.mainInventory_label);
+            this.panel2.Controls.Add(this.mainSales_label);
+            this.panel2.Controls.Add(this.mainInbox_label);
+            this.panel2.Location = new System.Drawing.Point(175, 39);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(166, 543);
+            this.panel2.TabIndex = 3;
+            // 
+            // mainReports_label
+            // 
+            this.mainReports_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainReports_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainReports_label.Location = new System.Drawing.Point(6, 164);
+            this.mainReports_label.Name = "mainReports_label";
+            this.mainReports_label.Size = new System.Drawing.Size(152, 29);
+            this.mainReports_label.TabIndex = 5;
+            this.mainReports_label.Text = "Reports";
+            this.mainReports_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainInventory_label
+            // 
+            this.mainInventory_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainInventory_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainInventory_label.Location = new System.Drawing.Point(6, 115);
+            this.mainInventory_label.Name = "mainInventory_label";
+            this.mainInventory_label.Size = new System.Drawing.Size(152, 29);
+            this.mainInventory_label.TabIndex = 4;
+            this.mainInventory_label.Text = "Inventory";
+            this.mainInventory_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainSales_label
+            // 
+            this.mainSales_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainSales_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainSales_label.Location = new System.Drawing.Point(6, 67);
+            this.mainSales_label.Name = "mainSales_label";
+            this.mainSales_label.Size = new System.Drawing.Size(152, 29);
+            this.mainSales_label.TabIndex = 3;
+            this.mainSales_label.Text = "Sales";
+            this.mainSales_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainInbox_label
+            // 
+            this.mainInbox_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainInbox_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainInbox_label.Location = new System.Drawing.Point(6, 18);
+            this.mainInbox_label.Name = "mainInbox_label";
+            this.mainInbox_label.Size = new System.Drawing.Size(152, 29);
+            this.mainInbox_label.TabIndex = 2;
+            this.mainInbox_label.Text = "Inbox";
+            this.mainInbox_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.mainWelcome_label);
+            this.panel3.Controls.Add(this.mainContent_label);
+            this.panel3.Location = new System.Drawing.Point(339, 39);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(577, 543);
+            this.panel3.TabIndex = 4;
+            // 
+            // mainTitleBar_panel
+            // 
+            this.mainTitleBar_panel.BackColor = System.Drawing.SystemColors.Highlight;
+            this.mainTitleBar_panel.Controls.Add(this.mainMinimise_panel);
+            this.mainTitleBar_panel.Controls.Add(this.mainClose_panel);
+            this.mainTitleBar_panel.Location = new System.Drawing.Point(-1, -1);
+            this.mainTitleBar_panel.Name = "mainTitleBar_panel";
+            this.mainTitleBar_panel.Size = new System.Drawing.Size(918, 43);
+            this.mainTitleBar_panel.TabIndex = 5;
+            this.mainTitleBar_panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainTitleBar_panel_MouseDown);
+            // 
+            // mainClose_panel
+            // 
+            this.mainClose_panel.AutoSize = true;
+            this.mainClose_panel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainClose_panel.Location = new System.Drawing.Point(888, 12);
+            this.mainClose_panel.Name = "mainClose_panel";
+            this.mainClose_panel.Size = new System.Drawing.Size(17, 17);
+            this.mainClose_panel.TabIndex = 0;
+            this.mainClose_panel.Text = "X";
+            this.mainClose_panel.Click += new System.EventHandler(this.mainClose_panel_Click);
+            // 
+            // mainMinimise_panel
+            // 
+            this.mainMinimise_panel.AutoSize = true;
+            this.mainMinimise_panel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainMinimise_panel.Location = new System.Drawing.Point(865, 12);
+            this.mainMinimise_panel.Name = "mainMinimise_panel";
+            this.mainMinimise_panel.Size = new System.Drawing.Size(13, 17);
+            this.mainMinimise_panel.TabIndex = 2;
+            this.mainMinimise_panel.Text = "-";
+            this.mainMinimise_panel.Click += new System.EventHandler(this.mainMinimise_panel_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(915, 582);
+            this.Controls.Add(this.mainTitleBar_panel);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "main";
             this.Text = "main";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.mainTitleBar_panel.ResumeLayout(false);
+            this.mainTitleBar_panel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label mainWelcome_label;
+        private System.Windows.Forms.Label mainContent_label;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel mainTitleBar_panel;
+        private System.Windows.Forms.Label mainInbox_label;
+        private System.Windows.Forms.Label mainReports_label;
+        private System.Windows.Forms.Label mainInventory_label;
+        private System.Windows.Forms.Label mainSales_label;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button mainExport_button;
+        private System.Windows.Forms.Button mainSettings_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label mainMinimise_panel;
+        private System.Windows.Forms.Label mainClose_panel;
     }
 }
--- PHP_Sales_Database/PHP_Sales_Database/main.cs
+++ PHP_Sales_Database/PHP_Sales_Database/main.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
 
 namespace PHP_Sales_Database
 {
     public partial class main : Form
     {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
         public main()
         {
             InitializeComponent();
+        }
+
+        private void mainTitleBar_panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void mainClose_panel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mainMinimise_panel_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
