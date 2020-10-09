6449a4c47dbacac2c11fcd3bdf052bddd0da0750
Monique Kuhn <monique.n.kuhn@gmail.com>
2020-10-09 12:38:58 +1100

    SignUp  Interface

--- PHP_Sales_Database/PHP_Sales_Database/Login.Designer.cs
+++ PHP_Sales_Database/PHP_Sales_Database/Login.Designer.cs
@@ -151,7 +151,7 @@
             this.button3.TabIndex = 11;
             this.button3.Text = "SignUp";
             this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.signUp);
             // 
             // Login
             // 
--- PHP_Sales_Database/PHP_Sales_Database/Login.cs
+++ PHP_Sales_Database/PHP_Sales_Database/Login.cs
@@ -58,7 +58,7 @@ namespace PHP_Sales_Database
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void signUp(object sender, EventArgs e)
         {
 
         }
